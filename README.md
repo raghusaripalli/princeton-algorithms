708f12ee63d39772f5f291b9f2e240cdb41ea323
raghusaripalli <rvs1604@gmail.com>
2020-05-01 23:28:57 +0530

    Added README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# princeton-algorithms" 
+# princeton-algorithms
+
+Click [here](https://www.coursera.org/learn/algorithms-part1) to view the course.
---
+++ src/Deque.java
@@ -0,0 +1,150 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Deque<Item> implements Iterable<Item> {
+
+    private int size;
+    private Node head, tail;
+
+    // construct an empty deque
+    public Deque() {
+        size = 0;
+        head = tail = null;
+    }
+
+    // is the deque empty?
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // return the number of items on the deque
+    public int size() {
+        return size;
+    }
+
+    // add the item to the front
+    public void addFirst(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException("Item can't be null");
+        }
+        Node newNode = new Node(item);
+        if (size == 0) {
+            tail = newNode;
+            head = newNode;
+        } else {
+            head.next = newNode;
+            newNode.prev = head;
+            head = newNode;
+        }
+        ++size;
+    }
+
+    // add the item to the back
+    public void addLast(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException("Item can't be null");
+        }
+        Node newNode = new Node(item);
+        if (size == 0) {
+            tail = newNode;
+            head = newNode;
+        } else {
+            tail.prev = newNode;
+            newNode.next = tail;
+            tail = newNode;
+        }
+        ++size;
+    }
+
+    // remove and return the item from the front
+    public Item removeFirst() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Can't remove from an empty queue");
+        }
+        Node removeNode = head;
+        if (head == tail) {
+            head = tail = null;
+        } else {
+            head = removeNode.prev;
+            head.next = null;
+        }
+        --size;
+        return removeNode.data;
+    }
+
+    // remove and return the item from the back
+    public Item removeLast() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Can't remove from an empty queue");
+        }
+        Node removeNode = tail;
+        if (head == tail) {
+            head = tail = null;
+        } else {
+            tail = removeNode.next;
+            tail.prev = null;
+        }
+        --size;
+        return removeNode.data;
+    }
+
+    // return an iterator over items in order from front to back
+    public Iterator<Item> iterator() {
+        return new Iterator<Item>() {
+            private Node iterator = head;
+
+            @Override
+            public boolean hasNext() {
+                return iterator != null;
+            }
+
+            @Override
+            public Item next() {
+                Item value = iterator.data;
+                iterator = iterator.prev;
+                return value;
+            }
+        };
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+        Deque<Integer> deque = new Deque<>();
+        deque.addFirst(1);
+        deque.addLast(2);
+        deque.addFirst(3);
+        System.out.println("Size: " + deque.size());
+        for (Integer integer : deque) {
+            System.out.print(integer + "-");
+        }
+        System.out.println("end");
+        System.out.println(deque.removeFirst());
+        deque.size();
+        System.out.println(deque.removeFirst());
+        deque.size();
+        System.out.println(deque.removeLast());
+        deque.size();
+        try {
+            deque.addFirst(null);
+        } catch (IllegalArgumentException iae) {
+            System.out.println(iae.getMessage());
+        }
+
+        try {
+            deque.removeLast();
+        } catch (NoSuchElementException nse) {
+            System.out.println(nse.getMessage());
+        }
+    }
+
+    class Node {
+        Item data;
+        Node prev, next;
+
+        Node(Item data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+}
