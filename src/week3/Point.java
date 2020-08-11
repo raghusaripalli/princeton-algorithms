0dcd8ee6b1ead6eae85dce4aa5b4f5fee3f8f118
raghusaripalli <rvs1604@gmail.com>
2020-08-11 23:19:30 +0530

    Week3: Point API

---
+++ src/week3/Point.java
@@ -0,0 +1,135 @@
+package week3;
+
+import edu.princeton.cs.algs4.StdDraw;
+
+import java.util.Comparator;
+
+public class Point implements Comparable<Point> {
+
+    private final int x;     // x-coordinate of this point
+    private final int y;     // y-coordinate of this point
+
+    /**
+     * Initializes a new point.
+     *
+     * @param x the <em>x</em>-coordinate of the point
+     * @param y the <em>y</em>-coordinate of the point
+     */
+    public Point(int x, int y) {
+        /* DO NOT MODIFY */
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Draws this point to standard draw.
+     */
+    public void draw() {
+        /* DO NOT MODIFY */
+        StdDraw.point(x, y);
+    }
+
+    /**
+     * Draws the line segment between this point and the specified point
+     * to standard draw.
+     *
+     * @param that the other point
+     */
+    public void drawTo(Point that) {
+        /* DO NOT MODIFY */
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    /**
+     * Returns the slope between this point and the specified point.
+     * Formally, if the two points are (x0, y0) and (x1, y1), then the slope
+     * is (y1 - y0) / (x1 - x0). For completeness, the slope is defined to be
+     * +0.0 if the line segment connecting the two points is horizontal;
+     * Double.POSITIVE_INFINITY if the line segment is vertical;
+     * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
+     *
+     * @param that the other point
+     * @return the slope between this point and the specified point
+     */
+    public double slopeTo(Point that) {
+        if (this.compareTo(that) == 0)
+            return Double.NEGATIVE_INFINITY;
+        if (this.y == that.y)
+            return 0;
+        if (this.x == that.x)
+            return Double.POSITIVE_INFINITY;
+
+        return (double) (that.y - this.y) / (that.x - this.x);
+    }
+
+    /**
+     * Compares two points by y-coordinate, breaking ties by x-coordinate.
+     * Formally, the invoking point (x0, y0) is less than the argument point
+     * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
+     *
+     * @param that the other point
+     * @return the value <tt>0</tt> if this point is equal to the argument
+     * point (x0 = x1 and y0 = y1);
+     * a negative integer if this point is less than the argument
+     * point; and a positive integer if this point is greater than the
+     * argument point
+     */
+    public int compareTo(Point that) {
+        if (that == null)
+            return 1;
+
+        if (this.y == that.y) {
+            return Integer.compare(this.x, that.x);
+        } else {
+            return Integer.compare(this.y, that.y);
+        }
+    }
+
+    /**
+     * Compares two points by the slope they make with this point.
+     * The slope is defined as in the slopeTo() method.
+     *
+     * @return the Comparator that defines this ordering on points
+     */
+    public Comparator<Point> slopeOrder() {
+        return (point1, point2) -> {
+            double slopePoint1 = slopeTo(point1);
+            double slopePoint2 = slopeTo(point2);
+            return Double.compare(slopePoint1, slopePoint2);
+        };
+    }
+
+
+    /**
+     * Returns a string representation of this point.
+     * This method is provide for debugging;
+     * your program should not rely on the format of the string representation.
+     *
+     * @return a string representation of this point
+     */
+    public String toString() {
+        /* DO NOT MODIFY */
+        return "(" + x + ", " + y + ")";
+    }
+
+    /**
+     * Unit tests the Point data type.
+     */
+    public static void main(String[] args) {
+        Point point1 = new Point(1, 5);
+        Point point2 = new Point(1, 6);
+        Point point3 = new Point(2, 5);
+        Point point4 = new Point(5, -1);
+        Point point5 = new Point(3, 4);
+
+        assert point1.slopeTo(point2) == Double.NEGATIVE_INFINITY;
+        assert point1.slopeTo(point3) == Double.POSITIVE_INFINITY;
+        assert point1.slopeTo(point4) == -1.5;
+
+        Comparator<Point> point1Comparator = point1.slopeOrder();
+        assert point1Comparator.compare(point5, point4) > 0;
+
+        point1.draw();
+        point1.drawTo(point5);
+    }
+}
