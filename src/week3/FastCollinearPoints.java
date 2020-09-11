359f06b076a92ef6c4db95398a7ac03f7e11e4cc
raghusaripalli <rvs1604@gmail.com>
2020-09-11 14:37:03 +0530

    Week 3 Assignment

--- src/week3/FastCollinearPoints.java
+++ src/week3/FastCollinearPoints.java
@@ -4,39 +4,56 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
-public class BruteCollinearPoints {
+public class FastCollinearPoints {
 
-    private int size;
-    private LineSegment[] lineSegments;
+    private final LineSegment[] segments;
 
-    public BruteCollinearPoints(Point[] points) {
-
+    public FastCollinearPoints(Point[] points) {
         nullAndEqualityCheck(points);
 
-        this.lineSegments = new LineSegment[2];
-        this.size = 0;
-
-        Arrays.sort(points);
-
-        for (int i = 0; i < points.length - 3; i++) {
-            for (int j = i + 1; j < points.length - 2; j++) {
-                for (int k = j + 1; k < points.length - 1; k++) {
-                    for (int m = k + 1; m < points.length; m++) {
-                        if (points[i].slopeTo(points[j]) == points[j].slopeTo(points[k]) &&
-                                points[j].slopeTo(points[k]) == points[k].slopeTo(points[m])) {
-
-                            // add item to array
-                            addLineSegment(new LineSegment(points[i], points[m]));
-
-                            points[i].drawTo(points[m]);
-                            StdDraw.show();
+        Point[] pointsCopySO = Arrays.copyOf(points, points.length);
+        Point[] pointsCopyNO = Arrays.copyOf(points, points.length);
+        ArrayList<LineSegment> segmentsList = new ArrayList<LineSegment>();
+        Arrays.sort(pointsCopyNO);
+        for (Point origin : pointsCopyNO) {
+            Arrays.sort(pointsCopySO);
+            Arrays.sort(pointsCopySO, origin.slopeOrder());
+            int count = 1;
+            Point lineBeginning = null;
+            for (int j = 0; j < pointsCopySO.length - 1; ++j) {
+                if (pointsCopySO[j].slopeTo(origin) == pointsCopySO[j + 1].slopeTo(origin)) {
+                    count++;
+                    if (count == 2) {
+                        lineBeginning = pointsCopySO[j];
+                        count++;
+                    } else if (count >= 4 && j + 1 == pointsCopySO.length - 1) {
+                        if (lineBeginning.compareTo(origin) > 0) {
+                            segmentsList.add(new LineSegment(origin, pointsCopySO[j + 1]));
                         }
+                        count = 1;
                     }
+                } else if (count >= 4) {
+                    if (lineBeginning.compareTo(origin) > 0) {
+                        segmentsList.add(new LineSegment(origin, pointsCopySO[j]));
+                    }
+                    count = 1;
+                } else {
+                    count = 1;
                 }
             }
         }
+        segments = segmentsList.toArray(new LineSegment[segmentsList.size()]);
+    }
+
+    public int numberOfSegments() {
+        return this.segments.length;
+    }
+
+    public LineSegment[] segments() {
+        return Arrays.copyOf(this.segments, numberOfSegments());
     }
 
     private void nullAndEqualityCheck(Point[] points) {
@@ -57,25 +74,6 @@ public class BruteCollinearPoints {
 
     private void cornerCase() {
         throw new IllegalArgumentException();
-    }
-
-    private void addLineSegment(LineSegment lineSegment) {
-        if (size == lineSegments.length) {
-            int capacity = 2 * lineSegments.length;
-            LineSegment[] temp = new LineSegment[capacity];
-            System.arraycopy(this.lineSegments, 0, temp, 0, this.size);
-            this.lineSegments = temp;
-        }
-
-        this.lineSegments[this.size++] = lineSegment;
-    }
-
-    public int numberOfSegments() {
-        return this.size;
-    }
-
-    public LineSegment[] segments() {
-        return Arrays.copyOf(this.lineSegments, this.size);
     }
 
     // Unit Tests
